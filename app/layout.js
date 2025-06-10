67b8616aaa0b35bc7d739933746bac0c043e2de1
dragang1 <gajicdragan22@gmail.com>
2025-06-10 10:47:30 +0200

    favicon error

--- app/layout.js
+++ app/layout.js
@@ -56,6 +56,11 @@ export const metadata = {
       sizes: "512x512",
       type: "image/png",
     },
+     {
+      url: "/favicon-32x32.png",
+      sizes: "32x32",
+      type: "image/png",
+    },
     {
       url: "/favicon.png",
       sizes: "48x48",
